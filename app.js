66819cd06fee3506589c9b376add010fe1ad40bd
Sampretha <samprethaprabhu@gmail.com>
2024-11-06 05:49:02 +0530

    Updated the greeting message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This is my Dockerized app!');
+  res.send('Hello World! This app is now updated!');
 });
 
 app.listen(port, () => {
